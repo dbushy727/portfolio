1e9d655d235a3100779b4bda53f0c0bc4f609bb7
dbushkanets <dbushkanets@harryfox.com>
2015-08-06 22:38:02 -0400

    cleaned up templates mail functionality

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,54 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
-	
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+/**
+ * check for empty/invalid data
+ * @param  array    $data   post data
+ * @return boolean
+ */
+function validData($data)
+{
+    if (empty($value)) {
+        echo "No arguments Provided!";
+        return false;
+    }
+
+    if ($key == "email" && !filter_var($value, FILTER_VALIDATE_EMAIL)) {
+        echo "Invalid Email Address";
+        return false;
+    }
+    return true;
+}
+
+/**
+ * send an email
+ * @param  string   $name    name from user
+ * @param  string   $email   email address from user
+ * @param  string   $phone   phone number from user
+ * @param  string   $message message from user
+ * @return void
+ */
+function sendMail($name, $email, $phone, $message)
+{
+    $headers        = "From: noreply@danielbushkanets.com\n";
+    $headers        .= "Reply-To: $email_address";
+    $to             = 'd.bushkanets@gmail.com';
+    $email_subject  = "Portfolio Contact Form:  $name";
+    $email_body     = "You have received a new message from your website contact form.\n\n"
+                    . "Here are the details:\n\n
+                        Name: $name\n\n
+                        Email: $email_address\n\n
+                        Phone: $phone\n\n
+                        Message:\n$message";
+    mail($to, $email_subject, $email_body, $headers);
+}
+
+if (!validData($_POST)) {
+    return false;
+}
+
+$name           = $_POST['name'];
+$email_address  = $_POST['email'];
+$phone          = $_POST['phone'];
+$message        = $_POST['message'];
+
+sendMail($name, $email_address, $phone, $message);
+return true;
